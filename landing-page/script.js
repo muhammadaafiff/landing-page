c6489e7188f7428305634d243460ebffa7d6456e
muhammadaafiff <90713542+muhammadaafiff@users.noreply.github.com>
2023-09-22 23:44:50 +0700

    Add files via upload

---
+++ landing-page/script.js
@@ -0,0 +1,60 @@
+// Tambahkan kode JavaScript kalian di file ini
+function handleGetFormData(){
+    let name = document.getElementById("name").value
+    let city = document.getElementById("city").value
+    let email = document.getElementById("email").value
+    let zipCode = document.getElementById("zip-code").value
+    let status = document.getElementById("status").checked
+    
+    return{
+        name,
+        city,
+        email,
+        zipCode,
+        status
+    }
+}
+
+function isNumber(input){
+    if(!isNaN(input)){
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+function checkboxIsChecked(){
+    return document.getElementById("status").checked
+}
+
+function validateFormData(formData){
+    console.log(formData)
+
+    if(formData !== null && isNumber (formData.zipCode) && checkboxIsChecked()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const pesanWarning = document.getElementById("warning")
+
+function submit(){
+    if (validateFormData(handleGetFormData())){
+        pesanWarning.innerHTML=""
+    }
+    else{
+        pesanWarning.innerHTML="Periksa form anda sekali lagi"
+    }
+}
+
+const myForm = document.getElementById("my-form")
+myForm.addEventListener('submit', function (event){
+    event.preventDefault()
+
+    submit()
+})
+
+
